351952b0f8f4bfaf3339a727652b2360ae574fe4
cbertolasio <cbertolasio@gmail.com>
2013-03-26 09:49:08 -0600

    removed sample code, added jar files for ormlite

--- everywod/src/com/everywod/data/DatabaseConfigUtil.java
+++ everywod/src/com/everywod/data/DatabaseConfigUtil.java
@@ -6,7 +6,12 @@ import com.j256.ormlite.android.apptools.OrmLiteConfigUtil;
 /**
  * 
  * @author bradlymatheson
- *  You will need to run this utility locally on your development box (not in an Android device), whenever you make a change to one of your data classes. This means that right now, this must be done by hand to keep the configuration file in sync with your database classes. To run the utility you will need to use the local Java runtime environment (JRE). Under eclipse, edit the "Run Configuration" for the utility, select the JRE tab, and select an alternative JRE (1.5 or 1.6). Your project’s JRE should be undefined since it is an Android application. You’ll also need to remove the Android bootstrap entry from the Classpath tab.
+ *  You will need to run this utility locally on your development box (not in an Android device), whenever you make a change to one of your data classes. 
+ *  This means that right now, this must be done by hand to keep the configuration file in sync with your database classes. 
+ *  To run the utility you will need to use the local Java runtime environment (JRE). 
+ *  Under eclipse, edit the "Run Configuration" for the utility, select the JRE tab, and select an alternative JRE (1.5 or 1.6). 
+ *  Your project’s JRE should be undefined since it is an Android application. 
+ *  You’ll also need to remove the Android bootstrap entry from the Classpath tab.
  */
 public class DatabaseConfigUtil extends OrmLiteConfigUtil {
 	private static final Class<?>[] classes = new Class[] { ExampleModel.class, };
